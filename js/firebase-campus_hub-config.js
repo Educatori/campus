0c1a39c02c4945a2ec62a8c55dfab88d7700f899
Educatori <educatori127@gmail.com>
2026-09-21 20:39:11 +0200

    Update firebase-campus_hub-config.js

--- js/firebase-campus_hub-config.js
+++ js/firebase-campus_hub-config.js
@@ -12,14 +12,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsI40TPF3XQjeJlsPmRKq4aFyO-4ceA1A",
-  authDomain: "campushub-90d60.firebaseapp.com",
-  databaseURL: "https://campushub-90d60-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "campushub-90d60",
-  storageBucket: "campushub-90d60.firebasestorage.app",
-  messagingSenderId: "318654288542",
-  appId: "1:318654288542:web:069cf435aeab017481e18f",
-  measurementId: "G-R5LPY2B2X5"
+    apiKey: "AIzaSyCujVjW7Aorn0illq_2w50u8oAgWGJEBRY",
+  authDomain: "cruscotto-722bc.firebaseapp.com",
+  databaseURL: "https://cruscotto-722bc-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cruscotto-722bc",
+  storageBucket: "cruscotto-722bc.firebasestorage.app",
+  messagingSenderId: "594198664103",
+  appId: "1:594198664103:web:cc50982af1a9fc757b5c82",
+  measurementId: "G-BVS9JG6HVC"
 };
 
 const app  = initializeApp(firebaseConfig);
