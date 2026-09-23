b5ee06dc789cc06fde995be8264dbfe39b878ca7
Educatori <educatori127@gmail.com>
2026-09-23 20:43:58 +0200

    Update firebase-campus_hub-config.js

--- js/firebase-campus_hub-config.js
+++ js/firebase-campus_hub-config.js
@@ -12,13 +12,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsI40TPF3XQjeJlsPmRKq4aFyO-4ceA1A",
+  apiKey: "chiave",
   authDomain: "campushub-90d60.firebaseapp.com",
   databaseURL: "https://campushub-90d60-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "campushub-90d60",
   storageBucket: "campushub-90d60.firebasestorage.app",
-  messagingSenderId: "318654288542",
-  appId: "1:318654288542:web:069cf435aeab017481e18f",
+  messagingSenderId: "123",
+  appId: "123",
   measurementId: "G-R5LPY2B2X5"
 };
 
@@ -29,4 +29,4 @@ const auth = getAuth(app);
 // Sessione limitata al tab corrente (chiudi il browser → logout)
 setPersistence(auth, browserSessionPersistence).catch(console.error);
 
-export { db, ref, get, child, set,  onValue, auth, signInWithEmailAndPassword, signOut, onAuthStateChanged };
+export { db, ref, get, child, set, update, onValue, auth, signInWithEmailAndPassword, signOut, onAuthStateChanged };
