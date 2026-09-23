2213ada00a1fac15e0a82b9b1e96623bb2d68e26
Educatori <educatori127@gmail.com>
2026-09-23 20:52:34 +0200

    Update firebase-campus_hub-config.js

--- js/firebase-campus_hub-config.js
+++ js/firebase-campus_hub-config.js
@@ -1,7 +1,7 @@
 /* firebase-campus_hub-config.js */
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase, ref, get, child,  set, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { getDatabase, ref, get, child, set, update, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 import {
     getAuth,
     signInWithEmailAndPassword,
@@ -12,13 +12,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "chiave",
+  apiKey: "AIzaSyCsI40TPF3XQjeJlsPmRKq4aFyO-4ceA1A",
   authDomain: "campushub-90d60.firebaseapp.com",
   databaseURL: "https://campushub-90d60-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "campushub-90d60",
   storageBucket: "campushub-90d60.firebasestorage.app",
-  messagingSenderId: "123",
-  appId: "123",
+  messagingSenderId: "318654288542",
+  appId: "1:318654288542:web:069cf435aeab017481e18f",
   measurementId: "G-R5LPY2B2X5"
 };
 
